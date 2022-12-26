dc8b02d8268bc4dc6149e63aed4a01b00dca5d4b
Siva Sai <medasivasai29@gmail.com>
2022-12-26 12:35:07 +0530

    changed canvas size using css and rendersize based on the display

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function main(){
     const renderer = new THREE.WebGLRenderer({canvas});
 
     const fov = 75;
-    const aspect = 2.35;
+    const aspect = 2;
     const near = 0.1;
     const far = 5;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
@@ -42,8 +42,29 @@ function main(){
         makeInstance(geometry, 0xaa8844, 2),
     ];
 
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize){
+            renderer.setSize(width,height,false);
+        }
+        return needResize;
+    }
+
     function render(time) {
         time *= 0.001;
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight; 
+            camera.updateProjectionMatrix();
+        }
+
+        // const canvas = renderer.domElement;
+        // camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        // camera.updateProjectionMatrix();
                 
         cudes.forEach((cude,ndx) => {
             const speed = 1 + ndx*.1;
